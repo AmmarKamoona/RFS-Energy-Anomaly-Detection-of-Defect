135fe597d70774425a6f84dcf8253aa9364e56e9
AmmarKamoona <46128325+AmmarKamoona@users.noreply.github.com>
2021-08-26 11:32:35 +1000

    Update MVTecAD_train_evaluation_github.py

--- code/MVTecAD_train_evaluation_github.py
+++ code/MVTecAD_train_evaluation_github.py
@@ -6,9 +6,10 @@ import numpy
 import torch
 import matplotlib.pyplot as plt
 import pandas as pd
-from mvtec_train import PossionMLE
+from utils.utilis import PossionMLE
 from utils.mvtec_utils import get_mvtec
-from utils import Possion_UnnorLoglike
+from utils.utilis import Possion_UnnorLoglike
+from utils.utilis import eval_MHD
 from sklearn.metrics import roc_auc_score, confusion_matrix, auc, roc_curve
 from sklearn.metrics import precision_recall_fscore_support as prf
 from torchvision import transforms as T
@@ -34,93 +35,7 @@ auc_mean_dataset_rfs_like = []
 object_names_list = []
 
 
-def eval_MHD(model, dataloaders, device, args, lamda_hat, card_mle):
-    """Testing RFS energy model"""
 
-    dataloader_train, testloader_set, card_trainer, testloader_set, trainloader_eval = dataloaders
-    train_mu, train_cov = model
-    train_mu = torch.tensor(train_mu, dtype=float).to(device)
-    train_cov = torch.tensor(train_cov, dtype=float).to(device)
-    print('Testing...')
-
-    # Obtaining Labels and energy scores for train data
-
-    from torch.distributions import multivariate_normal
-    gmm = multivariate_normal.MultivariateNormal(train_mu, train_cov)
-    from scipy.spatial.distance import mahalanobis
-
-    Mahal_test = []
-    RFS_energy_test = []
-    RFS_like_test= []
-    labels_test = []
-
-    args.pick_topk = 0.03
-    for batch in testloader_set:
-        x = batch[0]
-        y = batch[1]
-        card = batch[2]
-
-        x = x.float().to(device).squeeze()
-        cov_inv = np.linalg.inv(train_cov.detach().cpu().numpy())
-        dist = torch.tensor(
-            [mahalanobis(sample.cpu().numpy(), train_mu.cpu().numpy(), cov_inv) ** 2 for sample in x]).to(device)
-        Maha_dist = torch.tensor(
-            [mahalanobis(sample.cpu().numpy(), train_mu.cpu().numpy(), cov_inv) for sample in x]).to(device)
-        samples_log_like = gmm.log_prob(x)
-        # dist = torch.log(torch.tensor([mahalanobis(sample.cpu().numpy(), train_mu.cpu().numpy(), cov_inv) for sample in x]).to(device))
-        if args.rank_mahl:
-            pick_topk = args.pick_topk
-            topk = torch.round(card * pick_topk)
-            top_dist = torch.topk(dist, int(topk.numpy()))
-            dist = top_dist[0]
-            # card=torch.tensor(dist.shape)
-        sample_Mahal = Maha_dist
-        sample_energy = 0.5 * dist
-        card_loss_like = card_mle.compute_loss(lamda_hat, card.float().to(device), with_NLL=False)
-        # card_loss2 = card_mle.compute_loss(lamda_hat, card.float().to(device), with_NLL=True)
-        # new card_loss calculation using unnormlaized likeliihood
-        card_loss = Possion_UnnorLoglike(lamda_hat, card.float().to(device), with_NLL=False)
-        neg_card_loss = Possion_UnnorLoglike(lamda_hat, card.float().to(device), with_NLL=True)
-
-        # sum the loglilihood of all smaples based on NAive log
-        if 1:
-
-
-            samples_Mahal = torch.sum(sample_Mahal, dim=0).view(-1)
-            RFS_set_energy = torch.sum(sample_energy, dim=0).view(-1) + neg_card_loss.view(-1)
-            RFS_set_like = torch.sum(samples_log_like, dim=0).view(-1) + card_loss_like.view(-1) + (
-                            card.float().to(device=device) + 1).lgamma().view(-1)
-            card_sample_test = card_loss + (card.float().to(device=device) + 1).lgamma()
-
-
-        Mahal_test.append(samples_Mahal.detach().cpu())
-        RFS_energy_test.append(RFS_set_energy.detach().cpu())
-        labels_test.append(y)
-        RFS_like_test.append(RFS_set_like.detach().cpu())
-
-    Mahal_test = torch.cat(Mahal_test).numpy()
-    RFS_energy_test = torch.cat(RFS_energy_test).numpy()
-    labels_test = torch.cat(labels_test).numpy()
-    RFS_like_test = torch.cat(RFS_like_test).numpy()
-
-    auc_thre = 0
-
-    auc_mahal = roc_auc_score(labels_test, Mahal_test) * 100
-    auc_RFS_energy = roc_auc_score(labels_test, RFS_energy_test) * 100
-    auc_RFS_like = roc_auc_score(labels_test, RFS_like_test) * 100
-    # ploting the ROC AUC curve
-    fpr, tpr, thresholds = roc_curve(labels_test, RFS_energy_test, pos_label=1)
-
-    # display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=np.round(auc_score2, 1), estimator_name=args.object)
-    # display.plot()
-    # plt.show()
-    # plt.savefig(args.object)
-
-    print('ROC AUC score using sum of Mahalanobis distance : {:.2f}%'.format(auc_mahal))
-    print('ROC AUC score using RFS energy: {:.2f}%'.format(auc_RFS_energy))
-    print('ROC AUC score using RFS loglikelihood : {:.2f}%'.format(auc_RFS_like))
-
-    return auc_mahal, auc_RFS_energy , auc_RFS_like, fpr, tpr
 
 
 def Train_MHVD(args, data, device):
